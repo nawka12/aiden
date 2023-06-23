3ebf1e29fd3944631626c293d8ce555b93551a41
KayfaHaarukku <54880732+nawka12@users.noreply.github.com>
2023-06-23 13:35:46 +0700

    Adding timeout handler
    So the original 403 handler doesn't work. I make a timeout handler to handle that instead.

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ ydl_opts = {
     'format': 'bestaudio/best',
     'quiet': True,
     'format-sort': 'acodec:aac',
-    'cookies-from-browser': 'brave'
 }
 
 # Whisper model
@@ -19,8 +18,7 @@ model = whisper.load_model("base")
 
 # Output directory for audio fragments
 output_directory = "audio_fragments"
-if not os.path.exists(output_directory):
-    os.makedirs(output_directory)
+os.makedirs(output_directory, exist_ok=True)
 
 # GUI window
 window = tk.Tk()
@@ -61,18 +59,17 @@ def process_audio_chunk(start_time, end_time, livestream_url):
             command = f'ffmpeg -y -ss {start_time} -i "{audio_url}" -t {end_time - start_time} -vn -c:a libmp3lame -q:a 2 "{output_filename}"'
 
             # Use subprocess to capture console output
-            process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            _, error_output = process.communicate()
+            process = subprocess.run(command, shell=True, capture_output=True, text=True)
 
             if process.returncode != 0:
-                error_message = error_output.decode('utf-8')
-                window.after(0, lambda: print(f"An error occurred: {error_message}"))
+                error_message = process.stderr
+                window.after(0, lambda err=error_message: transcription_text.insert(tk.END, f"An error occurred: {err}\n\n"))
 
                 # Check if the error contains "403 Forbidden" or "Failed to open segment"
-                if "403 Forbidden" in error_message or "Failed to open segment" in error_message:
+                if "403 Forbidden" in error_message:
                     # Stop the audio processing if HTTP 403 Forbidden or Failed to open segment error occurs
                     stop_audio_processing.set()
-                    window.after(0, lambda: print("Transcription stopped due to YT-dlp error."))
+                    window.after(0, lambda: transcription_text.insert(tk.END, "Transcription stopped due to YT-dlp error.\n\n"))
                 return
 
             window.after(0, lambda: print(f"Audio fragment {start_time}-{end_time} downloaded and converted successfully!"))
@@ -83,17 +80,12 @@ def process_audio_chunk(start_time, end_time, livestream_url):
             # Transcribe the audio chunk using Whisper
             transcription = model.transcribe(audio_data)
 
-            # Translate with GTL
-            dest_language = dest_language_entry.get()
-            if not dest_language:
-                dest_language = "en"  # Fill with "en" if empty
+            # Translate with Google Translate
+            dest_language = dest_language_entry.get() or "en"  # Fill with "en" if empty
 
             translator = Translator()
             try:
-                translations = translator.translate(f"{transcription['text']}", dest=dest_language)
-                # Print to console
-                window.after(0, lambda: print(f"AUTO: {transcription['text']}"))
-                window.after(0, lambda: print(f"TL: {translations.text}"))
+                translations = translator.translate(transcription['text'], dest=dest_language)
 
                 # Update the GUI with the transcription
                 window.after(0, lambda: transcription_text.insert(tk.END, f"Transcription ({start_time}-{end_time}): {transcription['text']}\n\n"))
@@ -101,9 +93,10 @@ def process_audio_chunk(start_time, end_time, livestream_url):
                 window.after(0, transcription_text.see, tk.END)
             except Exception as e:
                 window.after(0, lambda err=e: print(f"An error occurred during translation: {str(err)}"))
-        except yt_dlp.DownloadError as e:
+        except yt_dlp.utils.DownloadError as e:
+            stop_audio_processing.set()
             window.after(0, lambda err=e: print(f"An error occurred: {str(err)}"))
-            if "403 Forbidden" in str(e) or "Failed to open segment" in str(e):
+            if "403 Forbidden" in str(e):
                 # Stop the audio processing if HTTP 403 Forbidden or Failed to open segment error occurs
                 stop_audio_processing.set()
                 window.after(0, lambda: print("Transcription stopped due to YT-dlp error."))
@@ -111,6 +104,54 @@ def process_audio_chunk(start_time, end_time, livestream_url):
     if current_chunk > 0:
         previous_chunk_filename = f"livestream_chunk_maiden_{current_chunk - 1}.mp3"
         os.remove(previous_chunk_filename)
+
+# Add a timeout parameter to the process_audio_chunk function
+def process_audio_chunk_with_timeout(start_time, end_time, livestream_url, timeout):
+    # Create a new thread for the download process
+    download_thread = threading.Thread(target=process_audio_chunk, args=(start_time, end_time, livestream_url))
+
+    # Start the download thread
+    download_thread.start()
+
+    # Wait for the download thread to finish or timeout
+    download_thread.join(timeout)
+
+    # If the download thread is still alive, it means it has timed out
+    if download_thread.is_alive():
+        window.after(0, lambda: print(f"Download timed out for audio fragment {start_time}-{end_time}"))
+        window.after(0, lambda: transcription_text.insert(tk.END, f"Download timed out for audio fragment {start_time}-{end_time}\n\n"))
+        # Terminate the download process by setting the stop_audio_processing flag
+        stop_audio_processing.set()
+
+# Modify the process_audio_chunks function to use the new process_audio_chunk_with_timeout function
+def process_audio_chunks(livestream_url):
+    start_time = 0
+    chunk_duration = 10  # Set the chunk duration to 10 seconds
+
+    while not stop_audio_processing.is_set():
+        end_time = start_time + chunk_duration
+
+        process_audio_chunk_with_timeout(start_time, end_time, livestream_url, timeout=30)
+
+        start_time = end_time
+
+        # Check if the livestream has finished using yt-dlp
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            try:
+                info = ydl.extract_info(livestream_url, download=False)
+                duration = info.get('duration')
+            except yt_dlp.utils.DownloadError as e:
+                print(f"An error occurred: {str(e)}")
+                break
+
+        if duration is not None and start_time >= duration:
+            break
+
+    # Reset the GUI state after the transcription is completed or stopped
+    window.after(0, lambda: youtube_link_entry.config(state=tk.NORMAL))
+    window.after(0, lambda: dest_language_entry.config(state=tk.NORMAL))
+    window.after(0, lambda: start_button.config(state=tk.NORMAL))
+    window.after(0, lambda: stop_button.config(state=tk.DISABLED))
 
 def start_transcription():
     global audio_thread
@@ -136,34 +177,7 @@ def stop_transcription():
         stop_button.config(state=tk.DISABLED)  # Disable the stop button temporarily
         transcription_text.delete('1.0', tk.END)  # Clear the transcription text
 
-def process_audio_chunks(livestream_url):
-    start_time = 0
-    chunk_duration = 10  # Set the chunk duration to 10 seconds
-
-    while not stop_audio_processing.is_set():
-        end_time = start_time + chunk_duration
-
-        process_audio_chunk(start_time, end_time, livestream_url)
-
-        start_time = end_time
-
-        # Check if the livestream has finished using yt-dlp
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            try:
-                info = ydl.extract_info(livestream_url, download=False)
-                duration = info.get('duration')
-            except yt_dlp.DownloadError as e:
-                print(f"An error occurred: {str(e)}")
-                break
-
-        if duration is not None and start_time >= duration:
-            break
-
-    # Reset the GUI state after the transcription is completed or stopped
-    window.after(0, lambda: youtube_link_entry.config(state=tk.NORMAL))
-    window.after(0, lambda: dest_language_entry.config(state=tk.NORMAL))
-    window.after(0, lambda: start_button.config(state=tk.NORMAL))
-    window.after(0, lambda: stop_button.config(state=tk.DISABLED))
+# Move the start_transcription function before the stop_transcription function
 
 # Start button
 start_button = tk.Button(window, text="Start", width=20, command=start_transcription)
